02e62034785e198b4ab433d289d539bf8c0b93c7
helenasf8 <helenadasilvafortunato.if@gmail.com>
2026-04-17 14:09:26 -0300

    feat: integração do Backend com Frontend

--- front-study/vite.config.js
+++ front-study/vite.config.js
@@ -6,13 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  urlPattern: /^http:\/\/localhost:8001\/.*/i,
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })

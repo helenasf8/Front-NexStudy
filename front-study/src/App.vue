67ed6430750faa0c0c7ee1deff61e659560ac4ca
helenasf8 <helenadasilvafortunato.if@gmail.com>
2026-04-17 15:27:08 -0300

    feat: Configuração do suporte a PWA

--- front-study/src/App.vue
+++ front-study/src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import MateriaList from './components/MateriaList.vue';
+import OfflineBanner from './components/OfflineBanner.vue';
 </script>
 
 
 <template>
+  <OfflineBanner />
   <MateriaList />
 </template>
